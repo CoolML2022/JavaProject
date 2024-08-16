abd9b4850130953c66323ec4095a389e353d1ea1
CoolML2022 <ich@vincentkrebs.de>
2024-08-16 17:40:04 +0200

    Added Root and factorial + fixed bug with multiple brackets with the same values

--- CalculatorUI.java
+++ CalculatorUI.java
@@ -4,89 +4,116 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CalculatorUI extends JFrame{
 
-    final private Font maiFont = new Font("Arial", Font.BOLD, 18);
-    final private String[] Operators = new String[]{"+", "-", "*", "/"};
-    JTextField inputTextField, outputTextField;
+    final private Font mainFont = new Font("Arial", Font.BOLD, 18);
+    final private String[] Operators = new String[]{"(", ")", ".", "+", "-", "*", "/", "^","√", "!"};
+    JTextField inputTextField, outputTextField;    
     private String mainString;    
     public void Initialize(){
-
+        mainString = null;
         //*********** Screen Panel ************//
         JLabel outputJLabel = new JLabel("Result");
-        outputJLabel.setFont(maiFont);
+        outputJLabel.setFont(mainFont);
         inputTextField = new JTextField();
-        inputTextField.setFont(maiFont);        
+        inputTextField.setFont(mainFont);        
         outputTextField = new JTextField();
-        outputJLabel.setFont(maiFont);
-        outputJLabel.setFont(maiFont);
+        outputJLabel.setFont(mainFont);
+        outputJLabel.setFont(mainFont);
         JPanel screenPanel = new JPanel();
         screenPanel.setLayout(new GridLayout(3, 1, 5, 5));
         screenPanel.add(inputTextField);
         screenPanel.add(outputJLabel);
         screenPanel.add(outputTextField);
-
         //********** Button Panel **********//
+        /*
+         * Number Buttons
+         */
         JButton[] numberButtons = new JButton[10];
         for(int i = 0; i < numberButtons.length; i++){
             String value = String.valueOf(i);
             numberButtons[i] = new JButton(String.valueOf(i));
-            numberButtons[i].setFont(maiFont);
+            numberButtons[i].setFont(mainFont);
+            numberButtons[i].setBackground(new Color(255, 255, 255));
             numberButtons[i].addActionListener((ActionEvent e) -> {
                 mainString = inputTextField.getText();
                 mainString = mainString + value;
                 inputTextField.setText(mainString);
             });
         }
+        /* ************** Operator Buttons ************** */
         JButton[] operatorButtons = new JButton[Operators.length];
         for(int i = 0; i < Operators.length; i++){
             String operator = Operators[i];
             operatorButtons[i] = new JButton(Operators[i]);
-            operatorButtons[i].setFont(maiFont);
+            operatorButtons[i].setFont(mainFont);
             operatorButtons[i].addActionListener((ActionEvent e) -> {
                 char[] inputField = inputTextField.getText().toCharArray();
-                if(!Arrays.asList(Operators).contains(inputField[inputField.length-1])){
+                System.out.println(mainString == null);
+                if(mainString == null){
                     mainString = inputTextField.getText();
                     mainString += operator;
+                    if("√".equals(operator)){
+                        mainString+= "(";
+                    }
+                    
+                    inputTextField.setText(mainString);
+                }          
+                else if(!Arrays.asList(Operators).contains(inputField[inputField.length-1])){
+                    mainString = inputTextField.getText();
+                    mainString += operator;
+                    if("√".equals(operator)){
+                        mainString+= "(";
+                    }
                     inputTextField.setText(mainString);
                 }            
             });
         }
         /* *******************Equals Button******************* */
         JButton equalsButton = new JButton("=");
-        equalsButton.setFont(maiFont);
+        equalsButton.setFont(mainFont);
+        
+        equalsButton.setBackground(new Color(0, 0, 0));
         equalsButton.addActionListener((ActionEvent e) -> {
             mainString = inputTextField.getText();
-            outputTextField.setText(Interpreter(mainString));            
+            mainString = mainString.replaceAll("\\s+","");
+            inputTextField.setText(mainString);
+            ScientificCalc calc = new ScientificCalc();
+            calc.InputString = mainString;
+            outputTextField.setText(String.valueOf(calc.ScientificCalc()));            
         });
+        equalsButton.setOpaque(true);
         /* ******************Clear Button********************* */
         JButton clearButton = new JButton("AC");
-        clearButton.setFont(maiFont);
+        clearButton.setFont(mainFont);
         clearButton.addActionListener((ActionEvent e) -> {
-            mainString = "";
+            mainString = null;
             inputTextField.setText(mainString);
             outputTextField.setText(" ");            
         });
         /* ************ Button Panel Initialize ************* */
         JPanel buttonsJPanel = new JPanel();
-        buttonsJPanel.setLayout(new GridLayout(4, 3, 5 ,5));
+        buttonsJPanel.setLayout(new GridLayout(6, 3, 5 ,5));
         for (JButton numberButton : numberButtons) {
             buttonsJPanel.add(numberButton);
-        }
-        buttonsJPanel.add(equalsButton);
-        buttonsJPanel.add(clearButton);
+        }                
         for (JButton operatorButton : operatorButtons) {
             buttonsJPanel.add(operatorButton);
         }
-
+        buttonsJPanel.add(equalsButton);
+        buttonsJPanel.add(clearButton);
+        /* ***************** Main Panel Initialize *************** */
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(new Color(128, 128, 255));
+        mainPanel.setBackground(new Color(91, 173, 235, 92));
         mainPanel.add(screenPanel, BorderLayout.NORTH);
         mainPanel.add(buttonsJPanel, BorderLayout.CENTER);        
         add(mainPanel);
@@ -95,66 +122,9 @@ public class CalculatorUI extends JFrame{
         setMinimumSize(new Dimension(300, 400));
         setVisible(true);
     }
-    //Interprets the final String to find the solution of the given math equation 
-    List<String> operators;
-    List<Float> numbers;
-    public String Interpreter (String mainString){
-        operators = new ArrayList<>();
-        numbers = new ArrayList<>();   
-        mainString += "+"; //initiate placeholder removed later ---->
-        char[] testCharaters = mainString.toCharArray();
-        String cont = "";
-        for(int i = 0; i < testCharaters.length; i++){
-            if(!Arrays.asList(Operators).contains(String.valueOf(testCharaters[i]))){
-                cont += String.valueOf(testCharaters[i]);
-            }
-            else{
-                numbers.add(Float.valueOf(cont));
-                cont = "";
-                operators.add(String.valueOf(testCharaters[i]));
-            }
-        }
-        operators.remove(operators.size()-1); //removing last operator bc placeholder
-        RekursiveMultiplikation();
-        RekursiveAddition();        
-        if(numbers.size()>1)
-            System.err.print("Something went wrong... unkown symbol");
-        System.out.println(numbers);
-        return String.valueOf(numbers.get(0));
-    }
-
-    public void RekursiveMultiplikation (){
-        for(int i = 0; i < operators.size(); i++){
-            if("*".equals(operators.get(i))) {                
-                numbers.set(i, numbers.get(i) * numbers.remove(i+1));
-                operators.remove(i);
-                RekursiveMultiplikation();
-            }
-            else if("/".equals(operators.get(i))){
-                numbers.set(i, numbers.get(i) / numbers.remove(i+1));
-                operators.remove(i);
-                RekursiveMultiplikation();
-            }
-        }
-    }
-
-    public void RekursiveAddition(){
-        for(int i = 0; i < operators.size(); i++){
-            if("+".equals(operators.get(i))) {                
-                numbers.set(i, numbers.get(i) + numbers.remove(i+1));
-                operators.remove(i);
-                RekursiveAddition();
-            }
-            else if("-".equals(operators.get(i))){
-                numbers.set(i, numbers.get(i) - numbers.remove(i+1));
-                operators.remove(i);
-                RekursiveAddition();
-            }
-        }
-    }
-
     public static void main(String[] args) {
         CalculatorUI calculatorUI = new CalculatorUI();
         calculatorUI.Initialize();
     }
 }
+
--- ScientificCalc.java
+++ ScientificCalc.java
@@ -2,53 +2,74 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 
 
 public class ScientificCalc {
     public String InputString;
-    final private String[] Operators = new String[]{"+", "-", "*", "/", "^"};
-    public void main(String[] args) {
-        String[] bracketStrings;
-        Double[] bracketvalues;
-        //String test = "22*3-345*3/2^4-45*5/3";
-        bracketStrings = SeperateBrackets(InputString);
-        bracketvalues = new Double[bracketStrings.length];
-        solveEquation(bracketStrings, bracketvalues);
-        
-    }
+    final private String[] Operators = new String[]{"+", "-", "*", "/", "^", "!"};
     public Double ScientificCalc(){
         String[] bracketStrings;
         Double[] bracketvalues;
         //String test = "22*3-345*3/2^4-45*5/3";
         bracketStrings = SeperateBrackets(InputString);
-        bracketvalues = new Double[bracketStrings.length];
-        
+        bracketvalues = new Double[bracketStrings.length]; 
+        Print("Bracketvalues lenght: " + bracketvalues.length);    
         return solveEquation(bracketStrings, bracketvalues);
     }
-
+    //(352646*567-(356-7))
     public String[] SeperateBrackets(String inputString){
         String[] Strings_to_resolve;
+        //
+        inputString = inputString.replace("!", "!1");
         if(!inputString.contains("(")&&!inputString.contains(")")){
             Strings_to_resolve = new String[1];
             Strings_to_resolve[0] = inputString;
-            Print("skipped");
+            Print("skipped");            
             return Strings_to_resolve;
+        }                
+        //Cleaning up the inputString:
+        //Adds a multiplication sign bewteen a number and an open bracket: e.g 45+4(3-5) -> 45+4*(3-5)
+        for(int i = 0; i<10; i++){
+            inputString = inputString.replace(i+"(", i+"*(");
         }
-        char[] inputChar = inputString.toCharArray();
+        inputString = inputString.replace(")(", ")*1*(");
+
+        char[] inputChar = inputString.toCharArray();        
+        boolean root = false;
+        String rootvalue = "";
+        String rootreplacement = "";
+        for(int i = 0; i < inputChar.length; i++){
+            if("√".equals(String.valueOf(inputChar[i])))
+                root = true;    
+            if(root == true){
+                rootvalue += String.valueOf(inputChar[i]);
+                if(")".equals(String.valueOf(inputChar[i]))){
+                    rootreplacement = rootvalue.replace("√", "");
+                    inputString = inputString.replace(rootvalue, rootreplacement + "^0.5");
+                    inputString = inputString.replace("√", "");
+                    rootvalue = "";
+                    root = false;
+                }
+            }   
+        }
+        inputChar = inputString.toCharArray();
         int numOfBracketsOpen = 0;
         int numOfBracketsClose = 0;
+        Print("InputString: "+ inputString);
         for(int i = 0; i < inputChar.length; i++){
             if("(".equals(String.valueOf(inputChar[i])))
                 numOfBracketsOpen++;
             else if(")".equals(String.valueOf(inputChar[i])))
-                numOfBracketsClose++;
+                numOfBracketsClose++;    
         }
         Strings_to_resolve = new String[numOfBracketsClose+1];
         if(numOfBracketsClose != numOfBracketsOpen){
-            System.err.printf("Int: %d, String: %s, UpperHex: %X", 23, "Test", 42);
             return Strings_to_resolve;
         }
+
+        ///(475645*454646*?0?) -> ?1?
         for(int j = 0; j < numOfBracketsClose; j++){
             String newstring = "";
             boolean openBracket = false;
@@ -61,17 +82,22 @@ public class ScientificCalc {
                 newstring += String.valueOf(inputChar[i]);
                 if(")".equals(String.valueOf(inputChar[i])) && openBracket == true){
                     Strings_to_resolve[j] = newstring;
-                    inputString = inputString.replace(newstring, "?"+j+"?");
+                    Print("newString " + newstring + " InputString " + inputString);
+                    int count = CountMatches(inputString, newstring);    
+                    //int count = 0;
+                    Print("InputString: " + inputString);
+                    Print("Count: " + count);
+                    if(count > 1)
+                        inputString = inputString.replaceFirst(Pattern.quote(newstring), "?"+j+"?");
+                    else
+                        inputString = inputString.replace(newstring, "?"+j+"?");
                     break;
                 }
             }
         }
         Strings_to_resolve[numOfBracketsClose] = inputString;
-        int i = 0;
-        for (String s : Strings_to_resolve) {            
-            Print("bra_" +i+ "_ " + s);
-            i++;
-            
+        for(String string: Strings_to_resolve){
+            Print(string);
         }
         return Strings_to_resolve;
     }
@@ -80,7 +106,13 @@ public class ScientificCalc {
         //Create Numberstring
         /*
          * First String in bracketString is the deepest nested Bracketequation -> no values : e.g. "?0?"
+         * 
+         * [0] 46547+356
+         * [1] (3536*?0?-4)
+         * ?1?
          */
+
+         //38425825*45258746+286-2532
         for(int y = 0; y < bracketString.length; y++){
             String equation = bracketString[y];
             Print(equation);
@@ -89,7 +121,7 @@ public class ScientificCalc {
             List<Double> numbers = new ArrayList<>();
             List<String> operators = new ArrayList<>();
             char[] equationChar = equation.toCharArray();
-            String value = "";
+            String value = "";            
             for(int i = 0; i<equationChar.length; i++){
                 if(Arrays.asList(Operators).contains(String.valueOf(equationChar[i]))){
                     numbers.add(Double.valueOf(value));
@@ -104,13 +136,18 @@ public class ScientificCalc {
                 else
                     value+=String.valueOf(equationChar[i]);
             }
+            //5! -> 5!1
             numbers.add(Double.valueOf(value));
             bracketvalues[y] = Calculate(numbers, operators);
+            Print("Bracketvalue: " + bracketvalues[y]);
         }
         Print("Solution: " + bracketvalues[bracketvalues.length-1]);
         return bracketvalues[bracketvalues.length-1];
     }
-
+    /*
+     * 
+     * 
+     */
     public Double Calculate(List<Double> numbers, List<String> operators){
         CalcData calcData = new CalcData();
         calcData.numbers = numbers;
@@ -121,8 +158,10 @@ public class ScientificCalc {
         calcData = RekursiveAddition(calcData);
         return calcData.numbers.get(0);
     }
+    //3353466
+    //
     public  CalcData RekursiveMultiplikation(CalcData calcData){
-        for(int i = 0; i < calcData.operators.size(); i++){
+        for(int i = 0; i < calcData.operators.size(); i++){            
             if("*".equals(calcData.operators.get(i))) {                
                 calcData.numbers.set(i, calcData.numbers.get(i) * calcData.numbers.remove(i+1));
                 calcData.operators.remove(i);
@@ -141,7 +180,7 @@ public class ScientificCalc {
             if("+".equals(calcData.operators.get(i))) {                
                 calcData.numbers.set(i, calcData.numbers.get(i) + calcData.numbers.remove(i+1));
                 calcData.operators.remove(i);
-                calcData = RekursiveAddition(calcData);
+                RekursiveAddition(calcData);
             }
             else if("-".equals(calcData.operators.get(i))){
                 calcData.numbers.set(i, calcData.numbers.get(i) - calcData.numbers.remove(i+1));
@@ -152,14 +191,39 @@ public class ScientificCalc {
         return calcData;
     }
     public CalcData RekursivePower(CalcData calcData){
-        for(int i = 0; i<calcData.operators.size(); i++){            
+        for(int i = 0; i<calcData.operators.size(); i++){ 
             if("^".equals(calcData.operators.get(i))){                          
                 calcData.numbers.set(i, Math.pow(calcData.numbers.get(i), calcData.numbers.remove(i+1)));
                 calcData.operators.remove(i);
                 RekursivePower(calcData);
+            }     
+            else if("!".equals(calcData.operators.get(i))){
+                double factorial = calcData.numbers.get(i);
+                double value = 1;
+                if(calcData.numbers.get(i)== 0){
+                    calcData.numbers.set(i, value);
+                }
+                else{
+                    for(int j = (int)factorial; j > 0; j--){
+                        value = value* j;
+                    }
+                    calcData.numbers.set(i, value);
+                    calcData.numbers.remove(i+1);
+                }
+                calcData.operators.remove(i);
             }
+            
         }
         return calcData;
+    }
+    public int CountMatches(String input, String countString){
+        int count = 0;
+        while(input.contains(countString)){
+            input = input.replaceFirst(Pattern.quote(countString), "");
+            Print("Input: " + input);
+            count++;
+        }
+        return count;
     }
     public  class CalcData{
         List<Double> numbers;
